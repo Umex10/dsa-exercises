34265cc935b2e4ffa1d9303b6ad6c91d498dc4b1
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2026-04-03 10:35:19 +0200

    fe

--- reverse-linked-list/BetterSolution.java
+++ reverse-linked-list/BetterSolution.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class BetterSolution {
     // public ListNode reverseList(ListNode head) {
 
     //    ListNode last = null;
--- reverse-linked-list/MySolution.java
+++ reverse-linked-list/MySolution.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class MySolution {
     // public ListNode reverseList(ListNode head) {
 
     //     int i = 0;
