cb391715705e5bd3a6ff488f1b9913c38b3b0887
Umejr Dzinovic <Umejr.Dzinovic@edu.fh-joanneum.at>
2026-04-05 23:21:17 +0200

    feat: add single exercise with doc

---
+++ single/MySolution.java
@@ -0,0 +1,21 @@
+package single;
+
+class MySolution {
+    public int singleNumber(int[] nums) {
+        
+        // [4,1,2,1,2] = 10
+
+        int res = 0;
+
+        for (int num : nums) {
+            res = res ^ num; 
+            // 0100 --> 4
+            // 0101 --> 1 
+            // 0111 --> 2
+            // 0110 --> 1
+            // 0100 --> 4 end resultat
+        }
+
+    return res;
+    }
+} 
